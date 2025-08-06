595a1e04fecf3813b41a0b75dc3ba0cd275d7d37
wathsan11 <wathsaninduwara@gmail.com>
2025-08-06 21:38:18 +0530

    SiththanDbSchema is Uploaded

---
+++ Siththam/Siththam/src/main/java/com/AtrSite/Siththam/SiththamApplication.java
@@ -0,0 +1,13 @@
+package com.AtrSite.Siththam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiththamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiththamApplication.class, args);
+	}
+
+}
---
+++ Siththam/Siththam/src/test/java/com/AtrSite/Siththam/SiththamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AtrSite.Siththam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiththamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
